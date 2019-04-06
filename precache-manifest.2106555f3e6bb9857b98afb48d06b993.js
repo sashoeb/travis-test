b74dce787fcc0fd028a460d5d27a0e7ae0b56c85
Shoeb Ahmed <shoeb.ahmed@boltiot.com>
2019-04-06 15:23:45 +0530

    Updates

---
+++ precache-manifest.2106555f3e6bb9857b98afb48d06b993.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis-test/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1c09f0c2aeaeeeedcad0",
+    "url": "/travis-test/my-app/static/js/runtime~main.a4d890bb.js"
+  },
+  {
+    "revision": "df598effb7fc2076311e",
+    "url": "/travis-test/my-app/static/js/main.6be5ff38.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/travis-test/my-app/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "df598effb7fc2076311e",
+    "url": "/travis-test/my-app/static/css/main.8a5d87cc.chunk.css"
+  },
+  {
+    "revision": "6e5ec824839554aa57467e061bef230f",
+    "url": "/travis-test/my-app/index.html"
+  }
+];
